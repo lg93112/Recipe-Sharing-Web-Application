ec72daa1db963e5b45e7b8695d2582022f310462
Lingsong Gao <lg5@illinois.com>
2018-09-15 15:14:18 -0400

    remove Project

--- Project/Project Files/current_user.py
+++
@@ -1,5 +0,0 @@
-#!/home/foodlicious/mypython/bin/python
-import sys
-user=sys.argv[1]
-def currentuser():
-    return int(user)
--- Project/project recipe/recommend_results.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $json_payload = json_decode($_GET['json_payload']);
-  $recipeid   = $json_payload['recipesid'];
- echo "<form name='form' action='/home/foodlicious/public_html/user_profile.php' method='post'>"
- echo "<input type='hidden' name='recommend' value='$recipeid'/>"
- echo "</form>"
-?>
